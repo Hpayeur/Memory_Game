168677f08ad1ef394e733d8badb1cd5079d931e9
Hpayeur <235752572+Hpayeur@users.noreply.github.com>
2025-12-09 17:45:29 -0700

    Stored Cards

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 const cards = document.querySelectorAll('.memory-card');
 
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
 function flipCard() {
 console.log ('Flipping cards');
 this.classList.toggle('flip');
+if (!hasFlippedCard) {
+    hasFlippedCard = true;
+        firstCard = this}
+        else{hasFlippedCard= false;
+            secondCard = this;
+            console.log(firstCard.dataset.framework);
+            console.log(secondCard.dataset.framework);
+        }
 }
 
 cards.forEach(card => card.addEventListener('click',flipCard));
+
+    // console.log({hasFlippedCard, firstCard});
+    // console.log({firstCard, secondCard});
