8eb766f6aad3aea75d9839e38251765d71a86de9
Hpayeur <235752572+Hpayeur@users.noreply.github.com>
2025-12-09 17:55:47 -0700

    Matching Cards

--- script.js
+++ script.js
@@ -4,19 +4,31 @@ let hasFlippedCard = false;
 let firstCard, secondCard;
 
 function flipCard() {
-console.log ('Flipping cards');
-this.classList.toggle('flip');
-if (!hasFlippedCard) {
-    hasFlippedCard = true;
-        firstCard = this}
-        else{hasFlippedCard= false;
-            secondCard = this;
-            console.log(firstCard.dataset.framework);
-            console.log(secondCard.dataset.framework);
+    console.log('Flipping cards');
+    this.classList.toggle('flip');
+
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+    } else {
+        hasFlippedCard = false;
+        secondCard = this;
+
+        if (firstCard.dataset.framework === secondCard.dataset.framework) {
+            firstCard.removeEventListener('click', flipCard);
+            secondCard.removeEventListener('click', flipCard);
+            console.log('Look We have a match');
+        } else {
+            setTimeout(() => {
+                firstCard.classList.remove('flip');
+                secondCard.classList.remove('flip');
+                console.log('Sorry, But this is not a match');
+            }, 1500);
         }
+    }
 }
 
-cards.forEach(card => card.addEventListener('click',flipCard));
+cards.forEach(card => card.addEventListener('click', flipCard));
 
-    // console.log({hasFlippedCard, firstCard});
-    // console.log({firstCard, secondCard});
+// console.log({hasFlippedCard, firstCard});
+// console.log({firstCard, secondCard});
